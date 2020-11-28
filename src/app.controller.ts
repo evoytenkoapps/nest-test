961c94961955b6e38d50f0b34faeea5e6fb4ece1
Евгений Войтенко <jozik.service@gmail.com>
2020-11-28 16:27:38 +0300

    fix env

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,8 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    const text = this.appService.getHello();
+    console.log('text: ', text);
+    return 'asdasd';
   }
 }
